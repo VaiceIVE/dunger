299ca03510d34bced9513faa4b7b46bd5bae3f54
romaiy <sokolovgit256@gmail.com>
2025-02-07 00:18:12 +0600

    feat(uw): добавил библиотеки и конфиг

--- packages/frontend/dnd-user-web/vite.config.ts
+++ packages/frontend/dnd-user-web/vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import stylexPlugin from 'vite-plugin-stylex-dev';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -7,10 +8,24 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [
     react(),
+    // https://stylexjs.com/docs/learn/installation/#compiler
+    stylexPlugin({
+      useCSSLayers: true
+    }),
     svgr(),
     // система импортов TypeScript (иначе он игнорирует baseUrl)
     tsconfigPaths({ root: './' })
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0
+        api: 'modern-compiler',
+        // https://sass-lang.com/documentation/breaking-changes/mixed-decls/
+        silenceDeprecations: ['mixed-decls']
+      }
+    }
+  },
   // абсолютные импорты в SCSS-файлах
   resolve: { alias: { styles: '/src/styles' } }
 });
