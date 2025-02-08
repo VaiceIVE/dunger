3baac5494144148b912e2be8ce92ae4b492d758c
romaiy <sokolovgit256@gmail.com>
2025-02-08 18:27:00 +0600

    feat(uw): инциализировал некоторые страницы

--- packages/frontend/dnd-user-web/src/routes/router.tsx
+++ packages/frontend/dnd-user-web/src/routes/router.tsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from 'react-router-dom';
 import { BaseLayout } from './_layouts/BaseLayout';
-import { MainPage } from './MainPage';
+import { BackgroundsPage } from './BackgroundsPage';
+import { BestiaryPage } from './BestiaryPage';
+import { ClassesPage } from './ClassesPage';
+import { FeatsPage } from './FeatsPage';
+import { HomePage } from './HomePage';
+import { SpeciesPage } from './SpeciesPage';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route Component={BaseLayout}>
-      <Route path={'/'} Component={MainPage} />
+      <Route path={'/'} Component={HomePage} />
+
+      <Route path={'/classes/:id?'} Component={ClassesPage} />
+      <Route path={'/species/:id?'} Component={SpeciesPage} />
+      <Route path={'/feats/:id?'} Component={FeatsPage} />
+      <Route path={'/backgrounds/:id?'} Component={BackgroundsPage} />
+
+      <Route path={'/bestiary'} Component={BestiaryPage} />
 
       {/* not found */}
       <Route path={'*'} Component={() => <Navigate to={'/'} replace />} />
