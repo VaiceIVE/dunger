7a90383b6c3891c3d0eb147661cd387ded3abbca
romaiy <sokolovgit256@gmail.com>
2025-02-17 11:25:23 +0600

    feat(ui): добавил Container

--- packages/frontend/lib/ui/index.ts
+++ packages/frontend/lib/ui/index.ts
@@ -2,6 +2,7 @@
  * Components
  */
 export * from './components/Button';
+export * from './components/Container';
 export * from './components/Flex';
 export * from './components/Grid';
 export * from './components/Spinner';
