5abcc3038830f9ca1a9c3679469e3b8042197a7c
romaiy <sokolovgit256@gmail.com>
2025-02-19 00:10:21 +0600

    fix(ui): добавил пропсы компонента в Grid

--- packages/frontend/lib/ui/components/Grid/_components/GridCol/GridCol.tsx
+++ packages/frontend/lib/ui/components/Grid/_components/GridCol/GridCol.tsx
@@ -1,15 +1,19 @@
-import { PropsWithChildren, ReactNode } from 'react';
+import { ComponentProps } from 'react';
 import * as stylex from '@stylexjs/stylex';
 import { StyleXStyles } from '@stylexjs/stylex';
 
-export interface GridColProps extends PropsWithChildren {
+export interface GridColProps extends Omit<ComponentProps<'div'>, 'style'> {
   span?: number;
 
   style?: StyleXStyles;
 }
 
-export const GridCol = ({ children, span = 1, style }: GridColProps) => {
-  return <div {...stylex.props(styles.root(span), style)}>{children}</div>;
+export const GridCol = ({ children, span = 1, style, ...props }: GridColProps) => {
+  return (
+    <div {...props} {...stylex.props(styles.root(span), style)}>
+      {children}
+    </div>
+  );
 };
 
 const styles = stylex.create({
