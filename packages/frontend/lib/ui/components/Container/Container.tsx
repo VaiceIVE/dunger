0a95bcd402189712fc16e39f0490de448ea8b1e6
romaiy <sokolovgit256@gmail.com>
2025-02-19 00:10:42 +0600

    fix(ui): добавил медиа запросы в контейнер

--- packages/frontend/lib/ui/components/Container/Container.tsx
+++ packages/frontend/lib/ui/components/Container/Container.tsx
@@ -18,7 +18,11 @@ export function Container({ children, style, asChild }: ContainerProps) {
 const styles = stylex.create({
   root: {
     marginHorizontal: 'auto',
-    maxWidth: '1077px',
+    maxWidth: {
+      default: 868,
+      '@media(min-width: 1024px)': 1077,
+      '@media(min-width: 1440px)': 1444
+    },
     padding: {
       default: '32px 0'
     },
