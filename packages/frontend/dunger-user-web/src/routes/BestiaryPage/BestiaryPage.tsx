a7b8e66e8e5b7b6ab29a35a22e515a52d36e2edd
romaiy <sokolovgit256@gmail.com>
2025-02-22 01:34:52 +0600

    feat(uw): компоненты для страницы бестиария

--- packages/frontend/dunger-user-web/src/routes/BestiaryPage/BestiaryPage.tsx
+++ packages/frontend/dunger-user-web/src/routes/BestiaryPage/BestiaryPage.tsx
@@ -1,6 +1,6 @@
 import * as stylex from '@stylexjs/stylex';
 import { useParams } from 'react-router-dom';
-import { Container, headers, Stack } from '@dunger/ui';
+import { Button, ButtonVariant, Container, Grid, headers, SearchIcon, Stack, TextInput } from '@dunger/ui';
 import { SplitViewLayout } from 'features/SplitViewLayout';
 import { BestiaryCard } from './_components/BestiaryCard';
 import { BestiaryList } from './_components/BestiaryList';
@@ -17,12 +17,22 @@ export const BestiaryPage = () => {
           <SplitViewLayout.Master>
             <Stack gap={29}>
               <h1 {...stylex.props(headers.h1Bold)}>Бестиарий</h1>
+
               <Stack gap={16}>
-                <div>Поиск</div>
+                <Grid gap={isActiveCreature ? 8 : 16}>
+                  <Grid.Col span={isActiveCreature ? 11 : 6}>
+                    <TextInput style={styles.input} placeholder="Поиск" leftSection={<SearchIcon />} />
+                  </Grid.Col>
+                  <Grid.Col span={isActiveCreature ? 1 : 6}>
+                    <Button variant={ButtonVariant.accentSecondary}>Фильтры</Button>
+                  </Grid.Col>
+                </Grid>
+
                 <BestiaryList isActiveCreature={isActiveCreature} />
               </Stack>
             </Stack>
           </SplitViewLayout.Master>
+
           <SplitViewLayout.Detail>
             <BestiaryCard />
           </SplitViewLayout.Detail>
@@ -31,3 +41,12 @@ export const BestiaryPage = () => {
     </main>
   );
 };
+
+const styles = stylex.create({
+  input: {
+    width: {
+      default: '100%',
+      ':is([aria-selected=true])': '100%'
+    }
+  }
+});
--- packages/frontend/dunger-user-web/src/routes/BestiaryPage/_components/BestiaryCard/BestiaryCard.tsx
+++ packages/frontend/dunger-user-web/src/routes/BestiaryPage/_components/BestiaryCard/BestiaryCard.tsx
@@ -1,9 +1,31 @@
 import * as stylex from '@stylexjs/stylex';
-import { Stack } from '@dunger/ui';
+import { Link } from 'react-router-dom';
+import { Flex, FolderIcon, headers, IconButton, LinkIcon, PencilIcon, Stack, XIcon } from '@dunger/ui';
 import { colors } from '@dunger/ui/tokens.stylex';
 
 export const BestiaryCard = () => {
-  return <Stack style={styles.root} gap={32}></Stack>;
+  return (
+    <Stack style={styles.root} gap={16}>
+      <Flex style={styles.header} gap={8}>
+        <h3 {...stylex.props(headers.h3Semibold, styles.name)}>Даратра Шендрел</h3>
+        <IconButton>
+          <PencilIcon />
+        </IconButton>
+        <IconButton>
+          <LinkIcon />
+        </IconButton>
+        <IconButton>
+          <FolderIcon />
+        </IconButton>
+        <Link to={'/bestiary'}>
+          <IconButton>
+            <XIcon />
+          </IconButton>
+        </Link>
+      </Flex>
+      <Stack style={styles.content} gap={32}></Stack>
+    </Stack>
+  );
 };
 
 const styles = stylex.create({
@@ -13,6 +35,29 @@ const styles = stylex.create({
     borderStyle: 'solid',
     borderWidth: 2,
     height: 'calc(100dvh - 64px)',
-    position: 'relative'
+    overflowY: 'scroll',
+    position: 'relative',
+    scrollbarGutter: 'stable',
+    '::-webkit-scrollbar': {
+      background: 'transparent',
+      display: 'none',
+      height: 0,
+      width: 0
+    }
+  },
+  header: {
+    backgroundColor: 'white',
+    left: 0,
+    padding: '24px 24px 12px',
+    position: 'sticky',
+    top: 0,
+    width: '100%'
+  },
+  name: {
+    flex: '1'
+  },
+  content: {
+    paddingBottom: 16,
+    paddingInline: 24
   }
 });
