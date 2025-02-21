39bbbfb00ad03ac6b2abddb4c2f233d98ed799b6
romaiy <sokolovgit256@gmail.com>
2025-02-21 21:39:20 +0600

    feat(ui): добавил иконку поиска

--- packages/frontend/lib/ui/components/Icon/index.ts
+++ packages/frontend/lib/ui/components/Icon/index.ts
@@ -1,6 +1,7 @@
 import FolderIcon from './_icons/folder.svg?react';
 import LinkIcon from './_icons/link.svg?react';
 import PencilIcon from './_icons/pencil.svg?react';
+import SearchIcon from './_icons/search.svg?react';
 import XIcon from './_icons/x.svg?react';
 
-export { FolderIcon, LinkIcon, PencilIcon, XIcon };
+export { FolderIcon, LinkIcon, PencilIcon, SearchIcon, XIcon };
