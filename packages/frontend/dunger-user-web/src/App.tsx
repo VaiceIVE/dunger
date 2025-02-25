0a2eff563e24a9cf31e82c1f2a3c175cae482e95
romaiy <sokolovgit256@gmail.com>
2025-02-26 00:32:05 +0600

    fix(uw): поддержал future react-router-dom

--- packages/frontend/dunger-user-web/src/App.tsx
+++ packages/frontend/dunger-user-web/src/App.tsx
@@ -2,5 +2,12 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from 'routes/router';
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider
+      future={{
+        v7_startTransition: true
+      }}
+      router={router}
+    />
+  );
 }
