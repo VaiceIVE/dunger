197d84849974c29015aa3105091b7b0f5eb71f1e
romaiy <sokolovgit256@gmail.com>
2025-03-06 23:59:54 +0600

    feat(ui): добавил иконки для SideBar

--- packages/frontend/lib/ui/components/Icon/index.ts
+++ packages/frontend/lib/ui/components/Icon/index.ts
@@ -1,9 +1,11 @@
 import BellFilledIcon from './_icons/bell-filled.svg?react';
+import DiceD20Icon from './_icons/dice-d20.svg?react';
 import FolderIcon from './_icons/folder.svg?react';
 import LinkIcon from './_icons/link.svg?react';
 import MenuIcon from './_icons/menu.svg?react';
 import PencilIcon from './_icons/pencil.svg?react';
+import PlusFilledIcon from './_icons/plus-filled.svg?react';
 import SearchIcon from './_icons/search.svg?react';
 import XIcon from './_icons/x.svg?react';
 
-export { BellFilledIcon, FolderIcon, LinkIcon, MenuIcon, PencilIcon, SearchIcon, XIcon };
+export { BellFilledIcon, DiceD20Icon, FolderIcon, LinkIcon, MenuIcon, PencilIcon, PlusFilledIcon, SearchIcon, XIcon };
