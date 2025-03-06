9f9baceee8ba5f2c0688db3050593b8d597b8125
romaiy <sokolovgit256@gmail.com>
2025-03-06 21:44:29 +0600

    feat(ui): добавил Tag

---
+++ packages/frontend/lib/ui/components/Tag/Tag.tsx
@@ -0,0 +1,34 @@
+import { PropsWithChildren } from 'react';
+import * as stylex from '@stylexjs/stylex';
+import { text } from '@utils/text';
+import { colors } from 'tokens.stylex';
+
+export interface TagProps extends PropsWithChildren {
+  color?: 'orange' | 'purple' | 'blue' | 'gray';
+}
+
+const styleByColor = {
+  orange: { backgroundColor: colors.orange5, color: colors.brand80 },
+  purple: { backgroundColor: colors.purple20, color: colors.purple80 },
+  blue: { backgroundColor: colors.blue10, color: colors.blue80 },
+  gray: { backgroundColor: colors.black2, color: colors.black70 }
+};
+
+export const Tag = ({ children, color = 'gray' }: TagProps) => {
+  return (
+    <div
+      {...stylex.props(text.smallMedium, styles.root(styleByColor[color].backgroundColor, styleByColor[color].color))}>
+      {children}
+    </div>
+  );
+};
+
+const styles = stylex.create({
+  root: (backgroundColor: string, color: string) => ({
+    backgroundColor,
+    borderRadius: 12,
+    color,
+    padding: '2px 6px',
+    width: 'fit-content'
+  })
+});
