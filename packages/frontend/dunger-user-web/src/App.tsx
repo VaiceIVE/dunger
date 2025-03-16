ad8072c9e2616361ad163bccb54a92a6cbe199bf
romaiy <sokolovgit256@gmail.com>
2025-03-16 19:32:58 +0600

    feat(uw): добавил провайдер для работы с сервером

--- packages/frontend/dunger-user-web/src/App.tsx
+++ packages/frontend/dunger-user-web/src/App.tsx
@@ -1,6 +1,19 @@
+import { Suspense } from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
+import { AuthFetchProvider } from '@dunger/auth-fetch';
 import { router } from 'routes/router';
+import { apiUrl, authUrl, basename } from 'store/config';
+import { queryClient } from 'store/queryClient';
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthFetchProvider basename={basename} authUrl={authUrl} apiUrl={apiUrl}>
+        <Suspense>
+          <RouterProvider router={router} />
+        </Suspense>
+      </AuthFetchProvider>
+    </QueryClientProvider>
+  );
 }
