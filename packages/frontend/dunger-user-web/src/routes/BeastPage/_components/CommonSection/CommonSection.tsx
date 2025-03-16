52029196eb2b25bf702c4c564789708ebf7d0997
romaiy <sokolovgit256@gmail.com>
2025-03-16 15:41:37 +0600

    fea(uw): добавил секции формы

--- packages/frontend/dunger-user-web/src/routes/BeastPage/_components/CommonSection/CommonSection.tsx
+++ packages/frontend/dunger-user-web/src/routes/BeastPage/_components/CommonSection/CommonSection.tsx
@@ -1,5 +1,24 @@
 import { Fragment } from 'react';
+import { Chips, SearchIcon, Select, Textarea, TextInput } from '@dunger/ui';
 
 export const CommonSection = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <TextInput label="Название существа" placeholder="Например, “Хобгоблин-вожак”" />
+      <Textarea
+        label="Описание"
+        placeholder={`Будучи Лордом Протектором Трибара и тайным агентом Арфистов, Даратра дала клятву защищать город.`}
+        minRows={8}
+        maxRows={12}
+        autosize
+      />
+      <Select placeholder="-Не выбрано-" label="Мировоззрение" />
+      <Select placeholder="-Не выбрано-" label="Тип" />
+      <Chips.Group label="Размер"></Chips.Group>
+      <Chips.Group label="Скорость"></Chips.Group>
+      <Select leftSection={<SearchIcon />} placeholder="Начните вводить" label="Языки владения" />
+      <Select leftSection={<SearchIcon />} placeholder="Начните вводить" label="Места обитания" />
+      <Chips.Group label="Чувства"></Chips.Group>
+    </Fragment>
+  );
 };
