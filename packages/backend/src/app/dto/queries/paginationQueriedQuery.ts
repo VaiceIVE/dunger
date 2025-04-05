58b60d73841d48594ef46ecb4abe8945ceef07bb
Ivan Isachenko <vanisachenko@gmail.com>
2025-04-05 16:58:28 +0600

    hotfix: Починил пагинацию

--- packages/backend/src/app/dto/queries/paginationQueriedQuery.ts
+++ packages/backend/src/app/dto/queries/paginationQueriedQuery.ts
@@ -1,9 +1,11 @@
 import { IsInt, IsOptional } from "class-validator"
 
 export class PaginationQureiedQuery{
+    
     @IsInt()
     @IsOptional()
     limit: number = 20
+
     @IsInt()
     @IsOptional()
     offset: number = 0
