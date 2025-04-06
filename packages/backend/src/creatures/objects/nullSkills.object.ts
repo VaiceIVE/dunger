ba29dbc35034e226fe01461abdf277a0efe9fadc
Ivan Isachenko <vanisachenko@gmail.com>
2025-04-06 20:38:48 +0600

    fix: Исправил выходной формат существа

---
+++ packages/backend/src/creatures/objects/nullSkills.object.ts
@@ -0,0 +1,86 @@
+import { ApiSkillsInput } from "../dto/stolen_types/ApiSkillsInput"
+
+export const nullSkillsObject = {
+    strength: {
+        athletics: {
+            value: null,
+            mastery: false
+            }
+      },
+      dexterity: {
+        acrobatics: {
+            value: null,
+            mastery: false
+            },
+        sleight_of_hand: {
+            value: null,
+            mastery: false
+            },
+        stealth: {
+            value: null,
+            mastery: false
+            }
+      },
+      intelligence: {
+        arcana: {
+            value: null,
+            mastery: false
+            },
+        history: {
+            value: null,
+            mastery: false
+            },
+        investigation: {
+            value: null,
+            mastery: false
+            },
+        nature: {
+            value: null,
+            mastery: false
+            },
+        religion: {
+            value: null,
+            mastery: false
+            }
+      },
+      wisdom: {
+        animal_handling: {
+            value: null,
+            mastery: false
+            },
+        insight: {
+            value: null,
+            mastery: false
+            },
+        medicine: {
+            value: null,
+            mastery: false
+            },
+        perception: {
+            value: null,
+            mastery: false
+            },
+        survival: {
+            value: null,
+            mastery: false
+            }
+      },
+      charisma: {
+        deception: {
+            value: null,
+            mastery: false
+            },
+        intimidation: {
+            value: null,
+            mastery: false
+            },
+        performance: {
+            value: null,
+            mastery: false
+            },
+        persuasion: {
+            value: null,
+            mastery: false
+            }
+      }
+} as ApiSkillsInput
