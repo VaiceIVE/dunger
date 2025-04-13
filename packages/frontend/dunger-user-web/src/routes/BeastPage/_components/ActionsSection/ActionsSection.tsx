f4b0a9513e3d1195551c56ce900198ac1aff4785
romaiy <sokolovgit256@gmail.com>
2025-04-13 17:58:13 +0600

    feat(uw): добавил и подключил к api секцию "Действия"

--- packages/frontend/dunger-user-web/src/routes/BeastPage/_components/ActionsSection/ActionsSection.tsx
+++ packages/frontend/dunger-user-web/src/routes/BeastPage/_components/ActionsSection/ActionsSection.tsx
@@ -1,10 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import * as stylex from '@stylexjs/stylex';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useAuthFetch } from '@dunger/auth-fetch';
 import {
+  Accordion,
   Button,
+  ButtonVariant,
+  ButtonWidth,
+  Chips,
+  Flex,
   headers,
   IconButton,
   IconButtonVariant,
+  InfiniteScroll,
   PlusFilledIcon,
   SearchIcon,
   Sheet,
@@ -14,19 +22,108 @@ import {
   XIcon
 } from '@dunger/ui';
 import { colors } from '@dunger/ui/tokens.stylex';
+import { ApiPaginatedResult } from 'store/_types/_common';
+import { ApiAction } from 'store/_types/ApiAction';
+import { useDebouncedValue } from 'utils/_hooks/useDebouncedValue';
 import { getWordForm } from 'utils/getWordForm';
+import { SectionProps } from '../BeastForm/BeastForm';
+import { GroupList } from './_components/GroupList';
 
-export const ActionsSection = () => {
-  const totalCount = 120;
+export const ActionsSection = ({ formState, handleFieldChange }: SectionProps) => {
+  const authFetch = useAuthFetch();
+
+  const [open, setOpen] = useState(false);
+  const [temp, setTemp] = useState<Record<string, ApiAction>>(
+    Object.fromEntries(formState.actions.map((action) => [action.name, action]))
+  );
+  const [openedSections, setOpenedSections] = useState<string[]>([]);
+
+  const [actionQuery, setActionQuery] = useState('');
+  const debouncedQuery = useDebouncedValue(actionQuery);
+
+  const {
+    data: actionGroups,
+    fetchNextPage: fetchMoreActionGroups,
+    hasNextPage: hasMoreActionGroups
+  } = useInfiniteQuery({
+    queryKey: ['creatures', 'action-groups', { query: debouncedQuery }],
+    queryFn: async ({ pageParam = 0 }) => {
+      const params = new URLSearchParams({ offset: pageParam.toString() });
+
+      if (debouncedQuery) {
+        params.set('query', debouncedQuery);
+      }
+
+      return authFetch<{ results: { name: string; count: number }[] } & ApiPaginatedResult>(
+        `/creatures/actions/groups?${params.toString()}`
+      );
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      const { offset, limit, totalCount } = lastPage.pagination;
+      const nextOffset = offset + limit;
+      return nextOffset < totalCount ? nextOffset : undefined;
+    }
+  });
+
+  const totalCount = actionGroups?.pages.length ? actionGroups.pages[0].pagination.totalCount : 0;
+  const actionGroupOptions = actionGroups?.pages.flatMap((p) => p.results) ?? [];
+
+  const handleSave = () => {
+    const actions = Object.values(temp);
+    handleFieldChange(actions, 'actions');
+    setOpen(false);
+  };
+
+  const handleReset = () => {
+    setTemp(Object.fromEntries(formState.actions.map((action) => [action.name, action])));
+  };
+
+  const handleChange = (actionObj: Record<string, ApiAction>) => {
+    setTemp((prev) => ({ ...prev, ...actionObj }));
+  };
+
+  const handleRemove = (actionName: string) => {
+    const { [actionName]: unused, ...filteredActions } = temp;
+    void unused;
+
+    setTemp(filteredActions);
+
+    handleFieldChange(Object.values(filteredActions), 'actions');
+  };
 
   return (
     <Fragment>
-      <Sheet>
+      <Sheet
+        open={open}
+        onOpenChange={(open) => {
+          if (!open) setOpenedSections([]);
+          setOpen(open);
+        }}>
+        <input type="hidden" name="actions_ids" value={formState.actions.map((a) => a.id).join(',')} />
+
         <Sheet.Trigger asChild>
           <Button type="button">
             Добавить действия <PlusFilledIcon {...stylex.props(styles.icon)} />
           </Button>
         </Sheet.Trigger>
+
+        {!!formState.actions.length && (
+          <Flex gap={8} rowGap={8}>
+            {formState.actions.map((a) => (
+              <Chips
+                onRemove={() => {
+                  handleRemove(a.name);
+                }}
+                withRemoveButton
+                style={text.defaultMedium}
+                value={a.id.toString()}
+                key={a.id}>
+                {a.name}
+              </Chips>
+            ))}
+          </Flex>
+        )}
         <Sheet.Content style={styles.root}>
           <Sheet.Header>
             <h3 {...stylex.props(headers.h3Semibold, styles.title)}>Действия</h3>
@@ -38,12 +135,47 @@ export const ActionsSection = () => {
           </Sheet.Header>
           <Stack style={styles.body} gap={24}>
             <Stack gap={16}>
-              <TextInput placeholder="Начните вводить" leftSection={<SearchIcon />} />
+              <TextInput
+                value={actionQuery}
+                onChange={(e) => {
+                  setActionQuery(e.target.value);
+                }}
+                placeholder="Начните вводить"
+                leftSection={<SearchIcon />}
+              />
               <div {...stylex.props(styles.count, text.smallSemibold)}>
                 Найдено: {totalCount} {getWordForm(totalCount, ['действие', 'действия', 'действий'])}
               </div>
             </Stack>
+
+            <Accordion
+              transitionDuration={1}
+              value={openedSections}
+              onChange={(e) => {
+                setOpenedSections(e as string[]);
+              }}
+              multiple>
+              <InfiniteScroll next={fetchMoreActionGroups} hasMore={hasMoreActionGroups} style={styles.list}>
+                <GroupList
+                  setValue={handleChange}
+                  value={temp}
+                  name={formState.name ? (formState.name === '' ? 'Существо' : formState.name) : 'Существо'}
+                  openedSections={openedSections}
+                  setOpenedSections={setOpenedSections}
+                  actionGroupOptions={actionGroupOptions}
+                />
+              </InfiniteScroll>
+            </Accordion>
           </Stack>
+
+          <Sheet.Footer style={styles.footer}>
+            <Button onClick={handleSave} width={ButtonWidth.full}>
+              Сохранить
+            </Button>
+            <Button onClick={handleReset} width={ButtonWidth.full} variant={ButtonVariant.ghost}>
+              Сбросить изменения
+            </Button>
+          </Sheet.Footer>
         </Sheet.Content>
       </Sheet>
     </Fragment>
@@ -52,6 +184,7 @@ export const ActionsSection = () => {
 
 const styles = stylex.create({
   root: {
+    color: colors.textPrimaryDefault,
     height: '100%',
     overflowY: 'scroll',
     scrollbarGutter: 'stable',
@@ -73,7 +206,16 @@ const styles = stylex.create({
     height: 16,
     width: 16
   },
+  list: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 14
+  },
   count: {
     color: colors.textTertiaryDefault
+  },
+  footer: {
+    display: 'flex',
+    gap: 8
   }
 });
---
+++ packages/frontend/dunger-user-web/src/routes/BeastPage/_components/ActionsSection/_components/ActionList/ActionList.tsx
@@ -0,0 +1,67 @@
+import { useRef } from 'react';
+import * as stylex from '@stylexjs/stylex';
+import { StyleXStyles } from '@stylexjs/stylex';
+import { useQuery } from '@tanstack/react-query';
+import { useAuthFetch } from '@dunger/auth-fetch';
+import { Flex, Radio, Spinner, Stack, text } from '@dunger/ui';
+import { colors } from '@dunger/ui/tokens.stylex';
+import { ApiAction } from 'store/_types/ApiAction';
+
+interface ActionListProps {
+  style?: StyleXStyles;
+
+  groupId: string;
+
+  opened: boolean;
+
+  name: string;
+
+  setValue: (actionObj: Record<string, ApiAction>) => void;
+}
+
+export const ActionList = ({ groupId, opened, name, style, setValue }: ActionListProps) => {
+  const hasBeenOpenedRef = useRef(opened);
+  hasBeenOpenedRef.current = opened || hasBeenOpenedRef.current;
+
+  const authFetch = useAuthFetch();
+
+  const { data: actions, isLoading } = useQuery({
+    queryKey: ['creatures', 'actions', groupId],
+    queryFn: () => authFetch<ApiAction[]>(`/creatures/actions/groups/${groupId}`),
+    enabled: hasBeenOpenedRef.current,
+    refetchOnWindowFocus: false
+  });
+
+  return (
+    <Stack gap={8} style={[styles.root, style]}>
+      {isLoading && <Spinner />}
+      {actions?.map((a) => (
+        <Stack style={styles.card} key={a.id} gap={10}>
+          <Flex align="flex-start" justify="space-between" wrap="nowrap" gap={12}>
+            <div {...stylex.props(text.smallMedium)}>{a.description.replaceAll('{name}', name)}</div>
+            <Radio
+              onClick={() => {
+                setValue({ [a.name]: a });
+              }}
+              size="sm"
+              value={a.id.toString()}
+            />
+          </Flex>
+        </Stack>
+      ))}
+    </Stack>
+  );
+};
+
+const styles = stylex.create({
+  root: {
+    padding: '12px'
+  },
+  card: {
+    borderColor: colors.outlinePrimaryDefault,
+    borderRadius: 10,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    padding: 14
+  }
+});
