d1b7c6bfbe1affca7613b3d31de8b4f3723ed80a
romaiy <sokolovgit256@gmail.com>
2025-04-20 01:31:50 +0600

    fix(c): поправил типы ApiCreatureInput

--- packages/backend/dunger-core/src/creatures/dto/stolen_types/ApiCreatureInput.ts
+++ packages/backend/dunger-core/src/creatures/dto/stolen_types/ApiCreatureInput.ts
@@ -9,12 +9,12 @@ export interface ApiCreatureInput {
   name: string;
   description: string | null;
   type_id: number | null;
-  alignment_id: string | null;
+  alignment_id: number | null;
   size_id: string | null;
   speed: ApiSpeedStat;
   languages_ids: number[];
-  languages?: {name: string}[];
-  biomes_ids: string[];
+  languages?: { name: string }[];
+  biomes_ids: number[];
   senses: ApiSenses;
   // Статблок
   hit_points: number;
@@ -25,12 +25,13 @@ export interface ApiCreatureInput {
   resistances_ids: string[];
   immunities_ids: string[];
   vulnerabilities_ids: string[];
-  alignment_relation?: {name: string}
-  type_relation?: {name: string}
-  size_relation?: {name: string}
-  biome_relation?: {title: string}[]
+  alignment_relation?: { name: string };
+  type_relation?: { name: string };
+  size_relation?: { name: string };
+  biome_relation?: { title: string }[];
   // Действия
   actions_ids: number[];
   // Способности и свойства
   traits_ids: number[];
-}``
+}
+``;
