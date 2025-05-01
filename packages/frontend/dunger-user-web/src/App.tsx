5abd807ac73cd91b34da348ea87e0324ecf1eb0a
romaiy <sokolovgit256@gmail.com>
2025-05-02 00:00:38 +0600

    fix(uw): поддержал изменения auth либы

--- packages/frontend/dunger-user-web/src/App.tsx
+++ packages/frontend/dunger-user-web/src/App.tsx
@@ -3,13 +3,13 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { AuthFetchProvider } from '@dunger/auth-fetch';
 import { router } from 'routes/router';
-import { apiUrl, authUrl, basename } from 'store/config';
+import { apiUrl } from 'store/config';
 import { queryClient } from 'store/queryClient';
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthFetchProvider basename={basename} authUrl={authUrl} apiUrl={apiUrl}>
+      <AuthFetchProvider apiUrl={apiUrl}>
         <Suspense>
           <RouterProvider router={router} />
         </Suspense>
