7dc4a36a707e9a42afebad9a9b41894f144badc2
romaiy <sokolovgit256@gmail.com>
2025-05-01 23:41:21 +0600

    fix(af): правки по fetch

--- packages/frontend/lib/auth-fetch/fetch/createAuthFetch.ts
+++ packages/frontend/lib/auth-fetch/fetch/createAuthFetch.ts
@@ -1,9 +1,11 @@
 export const createAuthFetch =
-  (apiUrl: string, getToken: () => Promise<string>) =>
+  (apiUrl: string, getToken?: () => Promise<string>) =>
   async <R>(endpoint: string, options?: RequestInit) => {
-    const accessToken = await getToken();
     const headers = new Headers(options?.headers);
-    headers.set('Authorization', `Bearer ${accessToken}`);
+    if (getToken) {
+      const accessToken = await getToken();
+      headers.set('Authorization', `Bearer ${accessToken}`);
+    }
     const response = await fetch(apiUrl + endpoint, { ...options, headers });
     if (!response.ok) throw Error(response.statusText);
     return (await response.json()) as Promise<R>;
