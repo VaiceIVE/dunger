95d6542bbf28774c9545fb6ee69cb66f87980fc3
romaiy <sokolovgit256@gmail.com>
2025-06-16 20:23:49 +0600

    feat(c): добавил OptionalJwtAuthGuard

---
+++ packages/backend/dunger-core/src/auth/guards/optional-jwt.guard.ts
@@ -0,0 +1,10 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+    // Просто возвращает user или undefined — без исключений
+    return user;
+  }
+}
